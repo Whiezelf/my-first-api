a35a5b172f2ae2fb781a48726fe38f9f405e1b4d
Whiezelf <vincespo19@gmail.com>
2025-12-15 17:12:56 +0100

    Debuging

--- app/main.py
+++ app/main.py
@@ -45,7 +45,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://my-first-api-frontend.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
